992373a6d42e53b4b769d416c9248d6359e690d4
gbarron2014 <barg90@hotmail.com>
2019-07-04 11:03:34 -0500

    Version 4 de Julio

--- src/main/java/com/utng/integradora/dao/IEstudianteDao.java
+++
@@ -1,7 +0,0 @@
-package com.utng.integradora.dao;
-
-import com.utng.integradora.entity.Estudiante;
-
-public interface IEstudianteDao extends IGenericDao<Estudiante, String> {
-
-}
--- src/main/java/com/utng/integradora/service/IEstudianteService.java
+++
@@ -1,7 +0,0 @@
-package com.utng.integradora.service;
-
-import com.utng.integradora.entity.Estudiante;
-
-public interface IEstudianteService extends IGenericService<Estudiante, String>{
-
-}
