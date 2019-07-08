380b22d617cd5bf496e653a3281fe204732e6dab
gbarron2014 <barg90@hotmail.com>
2019-07-08 16:53:07 -0500

    Version 8 de Julio

--- src/main/java/com/utng/course/dao/IMenuDao.java
+++ src/main/java/com/utng/course/dao/IMenuDao.java
@@ -2,6 +2,11 @@ package com.utng.course.dao;
 
 import com.utng.course.entity.Menu;
 
+/**
+ * Interface que añade más funcional si es posible al Menu
+ * @author Gabriel Barrón Rodríguez.
+ * @version 1.0
+ */
 public interface IMenuDao extends IGenericDao<Menu, Integer> {
 
 }
